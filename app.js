254b6906ac9ef18a7c0ac363d6c8d03fb3c1c021
JackieChiles <dtgaynor@gmail.com>
2015-12-06 17:53:04 -0500

    Added ICE page.

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.use(express.static('./'));
 app.get('/', function (req, res) {
     res.render('index.html');
 });
+app.get('/ice', function (req, res) {
+    res.render('ice.html');
+});
 
 server.listen(port, function () {
     console.log('Started on port %d...', port);
